11f037c543680d21cde8f44255adf1dab6863b30
Dario Carlino <baires82@gmail.com>
2020-09-12 00:08:07 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,6 +1,6 @@
 class Person
 
-  hash = {:name, :birthday, :hair_color, :eye_color, :height,
+  hash = {:birthday, :hair_color, :eye_color, :height,
 :weight, :handed, :complexion, :t_shirt_size,
 :wrist_size, :glove_size, :pant_length, :pant_width}
 end
